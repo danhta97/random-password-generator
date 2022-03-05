41470f63eff90e364b371f2ed2e7bc10fd75321a
danhta97 <danhta97@gmail.com>
2022-03-05 16:01:02 -0500

    Password Generator

---
+++ README.md
@@ -0,0 +1,24 @@
+# Random Password Generator
+
+## Purpose:
+Having secure password in this present time is extremely important to keep peoples informations safe. However with changing times, people need to update their passwords quite regularly as well.
+The purpose of this is to offer a program to randomly generate a new password with random characters for the most security possible. They can revisit the website whenever they need to change their password again and have a fresh new password under their belt.
+
+
+## How to Use:
+1. Choose wether you need Lowercase, Uppercase, Numbers, Special Characters.
+2. Click generate password button.
+3. Copy password.
+4. Use password however you please.
+
+
+## Made with:
+* Html
+* CSS
+* Javascript
+
+
+
+## Contribution:
+* GW Coding bootcamp 
+* Danh Ta
---
+++ assets/script.js
@@ -0,0 +1,71 @@
+
+var generatePassword = function() {
+  var password = ``;
+  var characterLength= passwordLength(); 
+  var characterType = passwordCharacters();
+
+
+  for (var i = 0; i < characterLength; i++) {
+    var mathMax = characterType.length;
+    var charNum = Math.floor(Math.random() * mathMax) + 1;
+    var charChoice = characterType.charAt(charNum);
+    password = password + charChoice;
+  }
+
+  return password;
+};
+
+var passwordLength = function() {
+
+  var length = parseInt(prompt("How many chracters for the password? Min 8, max 128."));
+  while (length < 8 || length > 128 || !Number.isInteger(length)) {
+    length = parseInt(prompt("Please enter valid number: 8 - 128."));
+  }
+  return length;
+};
+
+var passwordCharacters = function() {
+  var characters = ``;
+  var lowerCase = `abcdefghijklmnopqrstuvwxyz`;
+  var upperCase = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+  var specialChar = `!@#$%^&*()-=_'",<.>/?[{]}`;
+
+  while (!characters) {
+    alert(`Your password can contain at least one of the following: Lower Case, upper Case, and/or special Chracters.`);
+
+    var lowerConfirm = confirm(`Lower case characters? OK = YES Cancel = NO`);
+    if (lowerConfirm) {
+      characters = characters + lowerCase;
+    }
+
+    var upperConfirm = confirm(`Upper case characters? OK = YES Cancel = NO`);
+    if (upperConfirm) {
+      characters = characters + upperCase;
+    }
+
+    var specialConfirm = confirm(`Special characters? OK = YES Cancel = NO`);
+    if (specialConfirm) {
+      characters = characters + specialChar;
+    }
+
+    var confirmCharacters = confirm(`Confirm to generate password.`);
+    if (!confirmCharacters) {
+      characters = ``;
+      alert("Please make your character selections again.");
+    }
+  }
+
+  return characters;
+}
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
